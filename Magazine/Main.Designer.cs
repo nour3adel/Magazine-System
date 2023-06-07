38d8042f69f10d54159cf57c7e349a4146094786
Nour Adel <90010134+nour3adel@users.noreply.github.com>
2023-06-07 18:29:30 +0300

    Add files via upload

---
+++ Magazine/Main.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Magizine_project
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.pnl_header = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.hoverButton4 = new HoverButton();
+            this.hoverButton3 = new HoverButton();
+            this.hoverButton2 = new HoverButton();
+            this.hoverButton1 = new HoverButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.roundedButton4 = new RoundedButton();
+            this.pnl_header.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnl_header
+            // 
+            this.pnl_header.Controls.Add(this.label2);
+            this.pnl_header.Controls.Add(this.hoverButton4);
+            this.pnl_header.Controls.Add(this.hoverButton3);
+            this.pnl_header.Controls.Add(this.hoverButton2);
+            this.pnl_header.Controls.Add(this.hoverButton1);
+            this.pnl_header.Controls.Add(this.label1);
+            this.pnl_header.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnl_header.Location = new System.Drawing.Point(0, 0);
+            this.pnl_header.Name = "pnl_header";
+            this.pnl_header.Size = new System.Drawing.Size(1238, 70);
+            this.pnl_header.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(133, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 32);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Project";
+            // 
+            // hoverButton4
+            // 
+            this.hoverButton4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.hoverButton4.FlatAppearance.BorderSize = 0;
+            this.hoverButton4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.hoverButton4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.hoverButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.hoverButton4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hoverButton4.HoverColor = System.Drawing.Color.Chocolate;
+            this.hoverButton4.Location = new System.Drawing.Point(1207, 3);
+            this.hoverButton4.Name = "hoverButton4";
+            this.hoverButton4.Size = new System.Drawing.Size(28, 30);
+            this.hoverButton4.TabIndex = 20;
+            this.hoverButton4.Text = "X";
+            this.hoverButton4.UseVisualStyleBackColor = true;
+            this.hoverButton4.Click += new System.EventHandler(this.hoverButton4_Click);
+            // 
+            // hoverButton3
+            // 
+            this.hoverButton3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.hoverButton3.FlatAppearance.BorderSize = 0;
+            this.hoverButton3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.hoverButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.hoverButton3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hoverButton3.HoverColor = System.Drawing.Color.LightCoral;
+            this.hoverButton3.Location = new System.Drawing.Point(995, 19);
+            this.hoverButton3.Name = "hoverButton3";
+            this.hoverButton3.Size = new System.Drawing.Size(160, 42);
+            this.hoverButton3.TabIndex = 19;
+            this.hoverButton3.Text = "ABOUT";
+            this.hoverButton3.UseVisualStyleBackColor = true;
+            this.hoverButton3.Click += new System.EventHandler(this.hoverButton3_Click);
+            // 
+            // hoverButton2
+            // 
+            this.hoverButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.hoverButton2.FlatAppearance.BorderSize = 0;
+            this.hoverButton2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.hoverButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.hoverButton2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hoverButton2.HoverColor = System.Drawing.Color.LightCoral;
+            this.hoverButton2.Location = new System.Drawing.Point(819, 19);
+            this.hoverButton2.Name = "hoverButton2";
+            this.hoverButton2.Size = new System.Drawing.Size(160, 42);
+            this.hoverButton2.TabIndex = 18;
+            this.hoverButton2.Text = "THE TEAM";
+            this.hoverButton2.UseVisualStyleBackColor = true;
+            this.hoverButton2.Click += new System.EventHandler(this.hoverButton2_Click);
+            // 
+            // hoverButton1
+            // 
+            this.hoverButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.hoverButton1.FlatAppearance.BorderSize = 0;
+            this.hoverButton1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.hoverButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.hoverButton1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hoverButton1.ForeColor = System.Drawing.Color.Black;
+            this.hoverButton1.HoverColor = System.Drawing.Color.LightCoral;
+            this.hoverButton1.Location = new System.Drawing.Point(630, 19);
+            this.hoverButton1.Name = "hoverButton1";
+            this.hoverButton1.Size = new System.Drawing.Size(160, 42);
+            this.hoverButton1.TabIndex = 17;
+            this.hoverButton1.Text = "HOME";
+            this.hoverButton1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Myanmar Text", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 85);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "SWE";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(588, 111);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(738, 527);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Agency FB", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(63, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(303, 97);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "WELCOME";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Agency FB", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label5.Location = new System.Drawing.Point(116, 291);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(243, 77);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "MAGAZINE";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Agency FB", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(74, 306);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 34);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "TO";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Agency FB", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(101, 368);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(258, 97);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "SYSTEM";
+            // 
+            // roundedButton4
+            // 
+            this.roundedButton4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.roundedButton4.CornerRadius = 25;
+            this.roundedButton4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.roundedButton4.FlatAppearance.BorderSize = 0;
+            this.roundedButton4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(158)))));
+            this.roundedButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.roundedButton4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roundedButton4.ForeColor = System.Drawing.Color.White;
+            this.roundedButton4.Location = new System.Drawing.Point(63, 500);
+            this.roundedButton4.Name = "roundedButton4";
+            this.roundedButton4.Size = new System.Drawing.Size(335, 89);
+            this.roundedButton4.TabIndex = 16;
+            this.roundedButton4.Text = "GET STARTED";
+            this.roundedButton4.UseVisualStyleBackColor = false;
+            this.roundedButton4.Click += new System.EventHandler(this.roundedButton4_Click);
+            // 
+            // Main
+            // 
+            this.AcceptButton = this.roundedButton4;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.GhostWhite;
+            this.ClientSize = new System.Drawing.Size(1238, 685);
+            this.Controls.Add(this.roundedButton4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pnl_header);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "  ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnl_header.ResumeLayout(false);
+            this.pnl_header.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel pnl_header;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private RoundedButton roundedButton4;
+        private HoverButton hoverButton1;
+        private HoverButton hoverButton2;
+        private HoverButton hoverButton3;
+        private System.Windows.Forms.Label label2;
+        private HoverButton hoverButton4;
+    }
+}
+
---
+++ Magazine/Main.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using CrystalDecisions.CrystalReports.Engine;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+
+
+namespace Magizine_project
+{
+    public partial class Main : Form
+    {
+        #region Rounded Form
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(int nLeftRect,int nTopRect,int nRightRect,int nBottomRect,int nWidthEllipse,int nHeightEllipse);
+
+        #endregion
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        #region Home Form Load
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // make winform rounded
+            Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 50, 50));
+        }
+
+        #endregion
+
+        #region Exit
+        private void hoverButton4_Click(object sender, EventArgs e)
+        {
+            var result = RJMessageBox.Show("Close Application? ",
+                   "Alert",
+                   MessageBoxButtons.YesNo,
+                   MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        #endregion
+
+        #region Get Started Button
+
+        private void roundedButton4_Click(object sender, EventArgs e)
+        {
+            Login form = new Login();
+            form.Show();
+            form.Owner = this;
+            this.Hide();
+        }
+
+
+
+        #endregion
+
+        #region Team
+        private void hoverButton2_Click(object sender, EventArgs e)
+        {
+            Team form = new Team();
+            form.Show();
+            form.Owner = this;
+            this.Hide();
+        }
+
+        #endregion
+
+        #region About
+        private void hoverButton3_Click(object sender, EventArgs e)
+        {
+            About form = new About();
+            form.Show();
+            form.Owner = this;
+            this.Hide();
+        }
+
+        #endregion
+
+    }
+}
